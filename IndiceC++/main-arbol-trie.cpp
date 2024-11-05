143bde088aadfcad2d35bb28e061fe79f06c9d47
avillaq <villafuertequispealex@gmail.com>
2024-11-04 22:32:34 -0500

    Eliminacion de codigo inncesario

--- IndiceC++/main-arbol-trie.cpp
+++ IndiceC++/main-arbol-trie.cpp
@@ -33,7 +33,7 @@ public:
 
     // Insertar una palabra y el nombre de su archivo en el Trie
     void insertar(const string& palabra, const string& nombreArchivo) { 
-        Node* node = root; // nodo inicial
+        Node* node = root;
         for (char letra : palabra) { // para cada letra en nuestra palabra
             if (!node->children.count(letra)) { // si existe aun un hijo con esa letra
                 node->children[letra] = new Node(); // creamos un nuevo nodo hijo con dicha letra
@@ -45,7 +45,7 @@ public:
 
     // Buscar los archivos que contienen la palabra
     unordered_set<string> buscar(string& palabra) {
-        Node* node = root; // nodo inicial
+        Node* node = root; 
         for (char letra : palabra) { // para cada letra en nuestra palabra
             if (!node->children.count(letra)) { // si no existe un hijo con esa letra
                 return {}; // no existe, devolvemos un conjunto vacío
@@ -58,22 +58,6 @@ public:
 
 };
 
-// Función para recolectar los archivos de texto
-unordered_map<string, string> recolectarArchivos(vector<string> &nombresArchivos) {
-    unordered_map<string, string> archivosRecolectados; // mapa de archivos recolectados (nombre archivo - contenido)
-    for (string& nombre : nombresArchivos) {  // para cada archivo
-        ifstream archivoEntrada(nombre); // lo lee (modo lectura ifstream)
-        if (archivoEntrada) { // si se pudo abrir
-            stringstream texto; // se crea un stream para el texto
-            texto << archivoEntrada.rdbuf(); // se almacena el contenido en 'texto'
-            archivosRecolectados[nombre] = texto.str(); // el texto se almacena en el nombre del archivo en 'archivosRecolectados'
-        } else { // no se pudo abrir
-            cerr << "Error al abrir el archivo: " << nombre << endl; // imprime mensaje error
-        }
-    }
-    return archivosRecolectados; // retorna los archivos recolectados
-}
-
 // Función para eliminar signos de puntuación y saltos de linea
 string eliminarSignos(string& texto) {
     string nuevoTexto; // almacena el nuevo texto
@@ -84,7 +68,7 @@ string eliminarSignos(string& texto) {
             nuevoTexto += " "; // añade un espacio
         }
     }
-    return nuevoTexto; // retorna el nuevo texto
+    return nuevoTexto;
 }
 
 // Función para tokenizar un texto (separa palabra por palabra)
@@ -93,7 +77,7 @@ vector<string> tokenizarTexto(string& texto) {
     
     // istringstream sirve para tratar un string como si fuera un stream de entrada
     istringstream stream(texto); // se crea un stream para el texto
-    string palabra; // almacena una palabra
+    string palabra; 
 
     // Se extraen palabra por palabra del stream y se almacenan en la lista
     while (stream >> palabra) { // mientras se pueda extraer una palabra
@@ -104,17 +88,17 @@ vector<string> tokenizarTexto(string& texto) {
 
 // Función para eliminar palabras que no brindan informacion
 vector<string> eliminarStopWords(vector<string>& listaPalabras, unordered_set<string>& stopWords) {
-    vector<string> palabrasFiltradas; // almacena las palabras filtradas
+    vector<string> palabrasFiltradas;
     for (string& palabra : listaPalabras) { // para cada palabra en nuestra lista de palabras
         // Se verifica si la palabra no está en el conjunto de palabras vacías
         if (stopWords.find(palabra) == stopWords.end()) { // si esa palabra no esta en los StopWords
-            palabrasFiltradas.push_back(palabra); // la palabra se almacena en palabrasFiltradass
+            palabrasFiltradas.push_back(palabra);
         }
     }
-    return palabrasFiltradas; // retorna las palabras filtradas
+    return palabrasFiltradas;
 }
 
-// Estructura auxiliar para almacenar la palabra y el nombre del archivo
+// Estructura auxiliar para almacenar la palabra y el id de documento
 struct PalabraArchivo { 
     string palabra;
     string idDocumento;
@@ -122,13 +106,14 @@ struct PalabraArchivo {
 
 // Función para mapear los archivos procesados
 vector<PalabraArchivo> mapearArchivos(unordered_map<string, vector<string>>& archivosProcesados) {
-    PalabraArchivo pa; // usamos la estructura 'PalabraArchivo' para almacenar el nombre de archivo y la palabra
-    vector<PalabraArchivo> datosMappeados; // vector de palabraArchivo
-    // para cada nombre de archivo y lista de palabras en los archivos procesados
+    PalabraArchivo pa; 
+    vector<PalabraArchivo> datosMappeados;
+
+    // para cada id de documento y lista de palabras en los archivos procesados
     for (auto& [id_documento, listaPalabras] : archivosProcesados) { 
         for (string& palabra : listaPalabras) { // para cada palabra en la lista de palabras
-            pa.palabra = palabra; // la palabra se almacena en la palabra de la palabraArchivo
-            pa.idDocumento = id_documento; // el nombre de archivo se almacena en el id_documento de archivo de la palabraArchivo
+            pa.palabra = palabra;
+            pa.idDocumento = id_documento;
             datosMappeados.push_back(pa); // la palabraArchivo se almacena en datosMappeados
         }
     }
@@ -137,18 +122,17 @@ vector<PalabraArchivo> mapearArchivos(unordered_map<string, vector<string>>& arc
 
 // Organización de los datos intermedios: Agrupación por clave (palabra, palabra, ...)
 unordered_map<string, vector<string>> shuffle(vector<PalabraArchivo>& datosMapeados) {
-    unordered_map<string, vector<string>> datosAgrupados; // almacena los datos agrupados (palabra - nombre archivo)
-    for (auto& dato : datosMapeados) { // para cada palabraArchivo de nuestro vector de palabraArchivo
+    unordered_map<string, vector<string>> datosAgrupados; //  (palabra - id de documento)
+    for (auto& dato : datosMapeados) {
         datosAgrupados[dato.palabra].push_back(dato.idDocumento); // la palabra se almacena como clave y el id de documento como valor
-        // esta asignacion evita repeticiones
     }
-    return datosAgrupados; // retorna los datos agrupados
+    return datosAgrupados; 
 }
 
 // Reducir combinando listas de nombre de los archivos para cada palabra usando un Trie
 void reducirDatos(unordered_map<string, vector<string>>& datosAgrupados, Trie& trie) {
-    for (auto& [palabra, nombreArchivo] : datosAgrupados) { // para cada palabra y lista de archivos en los datos agrupados
-        for (auto& nom : nombreArchivo) { // para cada nombre de archivo por palabra
+    for (auto& [palabra, id_documentos] : datosAgrupados) {
+        for (auto& nom : id_documentos) {
             // insertamos la palabra y los nombres de los archivos a los que pertenece en el Trie
             trie.insertar(palabra, nom); 
         }
@@ -156,7 +140,7 @@ void reducirDatos(unordered_map<string, vector<string>>& datosAgrupados, Trie& t
 }
 
 unordered_set<string> procesarEntrada(Trie& trie ,string& entrada){ // procesa la entrada
-    istringstream stream(entrada); // se crea un stream para la entrada
+    istringstream stream(entrada);
     string palabra1; 
     stream >> palabra1; // se extrae la primer palabra
     string operador;
@@ -171,8 +155,8 @@ unordered_set<string> procesarEntrada(Trie& trie ,string& entrada){ // procesa l
         unordered_set<string> interseccionArchivos;
         for (const string& nombres : archivosEncontrados1) { // para los nombres encontrados en la primera busqueda
             // el metodo find devuelve un iterador al elemento si lo encuentra, si no, devuelve un iterador al final ( end() )
-            if (archivosEncontrados2.find(nombres) != archivosEncontrados2.end()) { // si el nombre del archivo esta en la segunda busqueda
-                interseccionArchivos.insert(nombres); // lo inserta en interseccionArchivos
+            if (archivosEncontrados2.find(nombres) != archivosEncontrados2.end()) {
+                interseccionArchivos.insert(nombres); 
             }
         }
         return interseccionArchivos; // retorna la interseccionArchivos
@@ -182,17 +166,16 @@ unordered_set<string> procesarEntrada(Trie& trie ,string& entrada){ // procesa l
         unordered_set<string> archivosEncontrados2 = trie.buscar(palabra2);
         
         archivosEncontrados1.merge(archivosEncontrados2); // combina los resultados de ambas busquedas
-        return archivosEncontrados1; // retorna los archivos encontrados
-    } else { // si no hay operador
+        return archivosEncontrados1;
+    } else {
         unordered_set<string> archivosEncontrados1 = trie.buscar(palabra1); // busca la primer palabra
-        return archivosEncontrados1; // retorna los archivos encontrados
+        return archivosEncontrados1;
     }
     
 }
 
 // Esta funcion se ejecutara en paralelo
 void crearIndiceInvertido(unordered_map<string, string> archivosRecolectados, int inicio, int fin, unordered_set<string>& stopWords) {
-    //unordered_map<string, string> archivosRecolectados = recolectarArchivos(nombresArchivos); 
     unordered_map<string, vector<string>> archivosProcesados;
 
     auto it = archivosRecolectados.begin();
